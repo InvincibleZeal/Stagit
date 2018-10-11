02cd2c3b8bcd21c15295ce50c2b695901f0c07ac
InvincibleZeal <ritesh.ganjewala17@gmail.com>
2018-10-12 03:22:01 +0530

    Updates

---
+++ precache-manifest.a8e0cd144180d00e02b913850d2a1a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/stagit/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/stagit/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "c5fb1304a94b2344416f",
+    "url": "/stagit/static/js/runtime~main.c5fb1304.js"
+  },
+  {
+    "revision": "2512288d4d6efa9991a1",
+    "url": "/stagit/static/js/main.2512288d.chunk.js"
+  },
+  {
+    "revision": "3b9391d3f8a1ae812c8b",
+    "url": "/stagit/static/js/1.3b9391d3.chunk.js"
+  },
+  {
+    "revision": "2512288d4d6efa9991a1",
+    "url": "/stagit/static/css/main.37205548.chunk.css"
+  },
+  {
+    "revision": "952bcae467fc9a9db7811095ded11fe7",
+    "url": "/stagit/index.html"
+  }
+];
