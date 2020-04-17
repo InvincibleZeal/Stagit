df851488bf18a159679c97c4c6848f51ee8b236a
Ritesh Ganjewala <ritesh.ganjewala17@gmail.com>
2020-04-17 22:42:35 +0530

    Finalizing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,62 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import Loadable from 'react-loadable';
 
-import About from './Components/About';
-import Home from './Components/Home';
-import Navbar from './Components/Navbar';
-import Support from './Components/Support';
-import User from './Components/User';
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Support from "./Components/Support";
+import User from "./Components/User";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       user: {},
-      validUser: false
+      validUser: false,
     };
-  }  
+  }
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/stagit">
         <div className="App">
           <Navbar />
           <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Home getUser={this.handleGetUser.bind(this)} />}
+            />
 
-            <Route exact path="/" render={() => (
-              <Home getUser={this.handleGetUser.bind(this)} />
-            )} />
-
-            <Route path="/user/:user" render={(object) => (
-              <User {...this.state} object={object} getUser={this.handleGetUser.bind(this)} />
-            )} />
+            <Route
+              path="/user/:user"
+              render={(object) => (
+                <User
+                  {...this.state}
+                  object={object}
+                  getUser={this.handleGetUser.bind(this)}
+                />
+              )}
+            />
 
             <Route exact path="/about" component={About} />
 
             <Route exact path="/support" component={Support} />
-
           </Switch>
         </div>
       </BrowserRouter>
     );
   }
-  
+
   handleGetUser = (user) => {
-    this.setState({validUser: true, user});
-  }
+    this.setState({
+      validUser: true,
+      user,
+    });
+  };
 }
 
 export default App;
